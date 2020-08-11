480cb2bd2ac1b31900aceb8ee1cb65a7db6a88a9
paulodiff <paulo.difficiliora@gmail.com>
2020-08-11 10:17:52 +0000

    aggiunti modifiche ai controlli

---
+++ controlli.js
@@ -0,0 +1,22 @@
+/**
+ * Modulo che implementa gli stessi controlli del modulo JSON
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+module.exports = {
+
+
+  ibanValidator : function( v ) {
+    console.log(v);
+  },
+
+  codiceFiscaleValidator : function(v) {
+    console.log(v);
+  }
+
+}
